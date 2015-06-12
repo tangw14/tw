22b1141970e72b7dfe3aebd7625c4a5f7313ec7c
tangw14 <540200196@qq.com>
2015-06-12 17:29:51 +0800

    test

---
+++ Desktop/hh/hh/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hh
+//
+//  Created by 唐蔚 on 15-5-7.
+//  Copyright (c) 2015年 唐蔚. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
